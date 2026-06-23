0124821bb08bb166fe4a91b2c0002a9e1b7822cf
bjjprogrammer <nigeryanes@gmail.com>
2026-06-23 00:49:21 -0500

    feat: enhance client creation logic with subscription handling and error management

--- src/actions/clients.ts
+++ src/actions/clients.ts
@@ -67,17 +67,58 @@ export async function updateClientAction(id: number, form: UpdateClient) {
 export async function createClientAction(form: CreateClient) {
   const supabase = await getSupabaseClient();
 
-  const { error } = await supabase.from('clients').insert({
-    first_name: form.first_name,
-    last_name: form.last_name,
-    email: form.email,
-    phone: form.phone,
-    plan_id: form.plan_id,
-    dni: form.dni,
-  });
+  const { data: client, error } = await supabase
+    .from('clients')
+    .insert({
+      first_name: form.first_name,
+      last_name: form.last_name,
+      email: form.email,
+      phone: form.phone,
+      plan_id: form.plan_id,
+      dni: form.dni,
+    })
+    .select()
+    .single();
 
   if (error) {
     return { success: false, error: error.message };
+  }
+
+  if (!client) {
+    return { success: false, error: 'Error al crear el cliente' };
+  }
+
+  const { data: plan, error: planError } = await supabase
+    .from('plans')
+    .select('*')
+    .eq('id', form.plan_id)
+    .single();
+
+  if (planError) {
+    return { success: false, error: planError.message };
+  }
+
+  if (!plan) {
+    return { success: false, error: 'Plan no encontrado' };
+  }
+
+  const startDate = new Date().toISOString();
+  const endDate = new Date(
+    new Date().setDate(new Date().getDate() + 30),
+  ).toISOString();
+
+  const { error: subscriptionError } = await supabase
+    .from('subscriptions')
+    .insert({
+      client_id: client.id,
+      plan_id: form.plan_id,
+      start_date: startDate,
+      end_date: endDate,
+      total_classes: plan.unlimited ? 99 : plan.total_classes, // TODO:  99 is a magic number for unlimited plans
+    });
+
+  if (subscriptionError) {
+    return { success: false, error: subscriptionError.message };
   }
 
   return { success: true };
